96b336f7321dd47cf82852975c398084b4f7bab0
NeZ-B <nezihebingol@gmail.com>
2021-12-20 23:49:11 +0100

    updated more hello faeture

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("developing to future");
+
+        System.out.println("Local repo is already published to github");
+
     }
 }
