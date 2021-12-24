f25554947357a4ac4134862bdea7da57dc83a3cd
NeZ-B <nezihebingol@gmail.com>
2021-12-24 22:53:06 +0100

    feature B is created

---
+++ src/test/java/featureB.java
@@ -0,0 +1,12 @@
+public class featureB {
+    int value;
+    String name;
+    double price;
+
+    public featureB(){
+        this.name=name;
+        this.price=price;
+        this.value=value;
+
+    }
+}
